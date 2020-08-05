b889b496f85cc1aac7018e26d73f013d847770d8
최홍희 <vvshinevv@tmon.co.kr>
2020-08-06 00:51:40 +0900

    leetcode: trie

---
+++ src/main/java/algorithms/online/leetcode/trie/Trie.java
@@ -0,0 +1,46 @@
+package algorithms.online.leetcode.trie;
+
+/**
+ * https://leetcode.com/problems/implement-trie-prefix-tree
+ *
+ * Your Trie object will be instantiated and called as such:
+ * Trie obj = new Trie();
+ * obj.insert(word);
+ * boolean param_2 = obj.search(word);
+ * boolean param_3 = obj.startsWith(prefix);
+ */
+class Trie {
+
+
+
+
+    /**
+     * Initialize your data structure here.
+     */
+    Trie() {
+
+    }
+
+    /**
+     * Inserts a word into the trie.
+     */
+    void insert(String word) {
+
+    }
+
+    /**
+     * Returns if the word is in the trie.
+     */
+    boolean search(String word) {
+        return false;
+    }
+
+    /**
+     * Returns if there is any word in the trie that starts with the given prefix.
+     */
+    boolean startsWith(String prefix) {
+        return false;
+    }
+}
+
+
---
+++ src/main/java/algorithms/online/leetcode/trie/TrieMain.java
@@ -0,0 +1,14 @@
+package algorithms.online.leetcode.trie;
+
+public class TrieMain {
+    public static void main(String[] args) {
+        Trie trie = new Trie();
+
+        trie.insert("apple");
+        trie.search("apple");   // returns true
+        trie.search("app");     // returns false
+        trie.startsWith("app"); // returns true
+        trie.insert("app");
+        trie.search("app");     // returns true
+    }
+}
