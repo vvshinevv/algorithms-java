8195de22e1802194402c7eacac6ed00635b7e34d
최홍희 <vvshinevv@gmail.com>
2020-08-14 20:57:56 +0900

    문제 설정 추가

--- src/main/java/algorithms/online/st11/problem2/Problem2.java
+++ src/main/java/algorithms/online/st11/problem2/Problem2.java
@@ -1,5 +1,15 @@
 package algorithms.online.st11.problem2;
 
+/**
+ * input은 배열 A와 숫자 K가 주어진다.
+ * 이때, 배열 A에 1, 2, 3, 4, 5, 6 ... K - 1, K 숫자가 최소한 1개는 존재해야 한다.
+ * 배열 A는 오름차순 정렬을 보장한다.
+ *
+ * ex1)
+ * A[] = { 1, 1, 2, 3 } K = 3
+ * K = 3 ( 1, 2, 3 ) 숫자가 배열 A에 모두 있으니 TRUE
+ *
+ */
 public class Problem2 {
     public static void main(String[] args) {
         boolean result1 = solution(new int[] { 1, 1, 2, 3, 3 }, 3);
