dbce81b94d7a76ddc2637444ac96b7aa9aee9ca2
최홍희 <vvshinevv@gmail.com>
2021-04-12 08:13:10 +0900

    더 맵게 풀었다~~ 야호~~

--- src/main/java/algorithms/online/programmers/heap/HeapSolve3.java
+++ src/main/java/algorithms/online/programmers/heap/HeapSolve3.java
@@ -1,5 +1,7 @@
 package algorithms.online.programmers.heap;
 
+import java.util.PriorityQueue;
+
 /**
  * 더 맵게 (X)
  * https://programmers.co.kr/learn/courses/30/lessons/42626
@@ -8,7 +10,7 @@ public class HeapSolve3 {
 
     public static void main(String[] args) {
         HeapSolve3 heapSolve3 = new HeapSolve3();
-
+        System.out.println(heapSolve3.solution(new int[]{0, 0}, 0)); // 0
         System.out.println(heapSolve3.solution(new int[]{1, 1, 2}, 3));
         System.out.println(heapSolve3.solution(new int[]{1, 1, 1}, 4)); // 2
         System.out.println(heapSolve3.solution(new int[]{10, 10, 10, 10, 10}, 100)); // 4
@@ -19,15 +21,34 @@ public class HeapSolve3 {
         System.out.println(heapSolve3.solution(new int[]{0, 0, 3, 9, 10, 12}, 7000)); // -1
         System.out.println(heapSolve3.solution(new int[]{0, 0, 3, 9, 10, 12}, 0)); // 0
         System.out.println(heapSolve3.solution(new int[]{0, 0, 3, 9, 10, 12}, 1)); // 2
-        System.out.println(heapSolve3.solution(new int[]{0, 0}, 0)); // 0
         System.out.println(heapSolve3.solution(new int[]{0, 0}, 1)); // -1
         System.out.println(heapSolve3.solution(new int[]{1, 0}, 1)); // 1
     }
 
     public int solution(int[] scoville, int K) {
 
+        PriorityQueue<Integer> heap = new PriorityQueue<>();
+        for (int i : scoville) {
+            heap.add(i);
+        }
 
-        return 0;
+        if (heap.size() == 0) {
+            return -1;
+        }
+        int count = 0;
+        Integer value = heap.poll();
+        while (value < K) {
+            if (heap.size() == 0) {
+                return -1;
+            }
+            count++;
+            Integer sec = heap.poll();
+            Integer mixed = value + (sec * 2);
+            heap.add(mixed);
+            value = heap.poll();
+        }
+
+        return count;
     }
 
 }
