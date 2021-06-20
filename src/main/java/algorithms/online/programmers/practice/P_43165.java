79a065c606d6e516cb7b83060cf3d4bc0e6d0f77
최홍희 <vvshinevv@gmail.com>
2021-06-20 15:27:43 +0900

    타겟넘버 풀기

--- src/main/java/algorithms/online/programmers/practice/P_43165.java
+++ src/main/java/algorithms/online/programmers/practice/P_43165.java
@@ -1,7 +1,5 @@
 package algorithms.online.programmers.practice;
 
-import java.util.Arrays;
-
 /**
  * 타겟 넘버
  * https://programmers.co.kr/learn/courses/30/lessons/43165
@@ -10,35 +8,23 @@ public class P_43165 {
 
     public static void main(String[] args) {
         P_43165 p_43165 = new P_43165();
-        //System.out.println(p_43165.solution(new int[]{1, 1, 1, 1, 1}, 3)); // 5
+        System.out.println(p_43165.solution(new int[]{1, 1, 1, 1, 1}, 3)); // 5
         System.out.println(p_43165.solution(new int[]{1, 2, 1, 2}, 2)); // 3
-//        System.out.println(p_43165.solution(new int[]{1, 2, 1, 2}, 6)); // 1
+        System.out.println(p_43165.solution(new int[]{1, 2, 1, 2}, 6)); // 1
     }
 
     public int solution(int[] numbers, int target) {
-
-        int n = numbers.length;
-        int answer = 0;
-
-        if (sum(numbers) == target) {
-            answer++;
-        }
-
-        for (int i = 0; i < n; i++) {
-            int[] copy = Arrays.copyOf(numbers, n);
-            for (int j = i ; j < n ; j++) {
-                copy[j] = copy[j] * -1;
-                if (sum(copy) == target) {
-                    answer++;
-                }
-            }
-
-        }
-
-        return answer;
+        return dfs(numbers[0], numbers, target, 0, 0) + dfs(numbers[0] * -1, numbers, target, 0, 0);
     }
 
-    public int sum(int[] numbers) {
-        return Arrays.stream(numbers).sum();
+    public int dfs(int node, int[] numbers, int target, int depth, int answer) {
+        if (numbers.length - 1 == depth) {
+            if (target == node) {
+                return answer + 1;
+            }
+            return 0;
+        }
+
+        return dfs(node + numbers[depth + 1], numbers, target, depth + 1, answer) + dfs(node + (numbers[depth + 1] * -1), numbers, target, depth + 1, answer);
     }
 }
